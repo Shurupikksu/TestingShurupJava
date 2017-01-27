9661dc6d2f31c2e52bcb4bb3f525b8ec30bacf7a
Shurupikksu <blairwitch@mail.ru>
2017-01-27 13:58:53 +0300

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/pftsandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pftsandbox/MyFirstProgramm.java
@@ -3,7 +3,7 @@ package ru.stqa.pftsandbox;
 public class MyFirstProgramm {
 
   public static void main(String[] args) {
-System.out.println("Hello world!");
+    System.out.println("Hello world!");
   }
 
 }
