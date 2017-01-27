6120e182f76057da45f64bab6256dadfff48833e
Shurupikksu <blairwitch@mail.ru>
2017-01-27 16:02:28 +0300

    классы и конструкторы объектов

---
+++ sandbox/src/main/java/ru/stqa/pftsandbox/Rectangle.java
@@ -0,0 +1,15 @@
+package ru.stqa.pftsandbox;
+
+/**
+ * Created by Ксюша on 27.01.2017.
+ */
+public class Rectangle {
+
+  public double a;
+  public double b;
+
+  public Rectangle(double r, double d){
+    this.a = r;
+    this.b = d;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/pftsandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.pftsandbox;
+
+/**
+ * Created by Ксюша on 27.01.2017.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+}
